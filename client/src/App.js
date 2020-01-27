69652978a0501743822e91c39571bdd48d1cd629
Joey Nevarez <josephnevarez12@gmail.com>
2020-01-26 16:15:50 -0800

    login page done

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" render={(props)=> <Login {...props}/>}/>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import api from '../utls/api';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '', 
+  });
+
+  const handleChange = (e) =>{
+    setCredentials({
+        ...credentials,
+        [e.target.name]: e.target.value
+    })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    api()
+        .post('/api/login', credentials)
+        .then((res)=>{
+            // console.log('from submit: ', res)
+            localStorage.setItem('token', res.data.payload)
+            props.history.push('/bubblepage')
+        })
+        .catch((err)=>{
+            console.log(err.response)
+            // setError(err.response.data)
+        })
+}
+
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <input type='username' name='username' placeholder='username' value={credentials.username} onChange={handleChange}/>
+        <input type='password' name='password' placeholder='Password' value={credentials.password} onChange={handleChange}/>
+        <button onClick={handleSubmit}>Sign Up</button>
+      </form>
     </div>
   );
 };
