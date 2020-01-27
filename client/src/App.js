78bb5331b23441048568e4db939271baec69e418
Joey Nevarez <josephnevarez12@gmail.com>
2020-01-26 16:20:09 -0800

    made privateroute for bubblepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import ProtectedRoute from './components/ProtectedRoute';
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <ProtectedRoute exact path='/bubblepage' component={BubblePage}/>
       </div>
     </Router>
   );
